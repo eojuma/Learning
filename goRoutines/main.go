bbe6147b532b89e007985fa7b240cd9456e5abc6
evajuma <evansjuma1e@gmail.com>
2026-03-04 02:38:26 +0300

    modified the main file so  that it takes into consideration the wait groups

--- goRoutines/main.go
+++ goRoutines/main.go
@@ -1,14 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+var waitGrpup sync.WaitGroup
 
 func main() {
-	for i := 0; i < 4; i++ {
-		 greeter("Hello")
-		 greeter("World")
+	// for i := 0; i < 4; i++ {
+	// 	 greeter("Hello")
+	// 	 greeter("World")
+	// }
+
+	websitelist := []string{
+		"https://youtube.com",
+		"https://fb.com",
+		"https://github.com",
+		"https://safaricom.com",
+		"https://go.dev",
 	}
+
+	for _, web := range websitelist {
+		go getStatusCode(web)
+		waitGrpup.Add(1)
+	}
+	waitGrpup.Wait()
 }
 
-func greeter(s string) {
-	fmt.Println(s)
+// func greeter(s string) {
+// 	fmt.Println(s)
+// }
+
+func getStatusCode(endpoint string) {
+
+	defer waitGrpup.Done()
+	res, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Println("Oops error", err)
+	}
+	fmt.Println(res.StatusCode, endpoint)
 }
