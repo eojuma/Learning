70862b3cc1b056613e53f21ba8abee931d69b4a6
evajuma <evansjuma1e@gmail.com>
2026-04-06 16:51:02 +0300

    feat: add more languages

--- hello.go
+++ hello.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
-
-
 )
+
 const englishPrefix = "Hello, "
+const spanishprefix = "Hola, "
+const spanish = "spanish,"
+const french = "french"
+const frenchprefix = "Bonjour,"
+const english = "english"
+
 func main() {
-fmt.Println(Hello(""))
+	fmt.Println(Hello("", ""))
 }
 
-func Hello(name string)string{
-	if name==""{
-		name="World!"
+func Hello(name string, language string) string {
+	if name == "" {
+		language = "english"
+		name = "World!"
 	}
-	return englishPrefix +  name
+	if language == "spanish" {
+		return spanishprefix + name
+	} else if language == "french" {
+		return frenchprefix + name
+	} else {
+
+		return englishPrefix + name
+	}
 }
