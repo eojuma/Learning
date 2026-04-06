09d702b978822063fabbe7eab1659dbc047716e9
evajuma <evansjuma1e@gmail.com>
2026-04-06 16:51:29 +0300

    test the added languages

--- hello_test.go
+++ hello_test.go
@@ -3,24 +3,35 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people",func(t *testing.T){
+	t.Run("saying hello to people", func(t *testing.T) {
 
-	got := Hello("Evans!")
-	want := "Hello, Evans!"
+		got := Hello("Evans!", "english")
+		want := "Hello, Evans!"
 
-	assertMessage(t,got,want)
-})
-t.Run("saying 'Hello,World',when an empty string is passed",func(t *testing.T){
-	got:=Hello("")
-	want:="Hello, World!"
-		assertMessage(t,got,want)
+		assertMessage(t, got, want)
+	})
+	t.Run("saying 'Hello,World',when an empty string is passed", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World!"
+		assertMessage(t, got, want)
 
-})
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Eldoie", "spanish")
+		want := "Hola, Eldoie"
+		assertMessage(t, got, want)
+	})
+
+	t.Run("saying hello in french", func(t *testing.T) {
+		got := Hello("Juma", "french")
+		want := "Bonjour,Juma"
+		assertMessage(t, got, want)
+	})
 }
-func assertMessage(t testing.TB,got,want string){
+func assertMessage(t testing.TB, got, want string) {
 	t.Helper()
-if got !=want{
-		t.Errorf("got %s,want %s",got,want)
+	if got != want {
+		t.Errorf("got %s,want %s", got, want)
 	}
 }
-
