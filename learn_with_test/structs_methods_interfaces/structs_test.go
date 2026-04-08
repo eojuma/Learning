9c94b71e1250ff9cd8878de1a367cecffe5e6495
evajuma <evansjuma1e@gmail.com>
2026-04-08 12:07:55 +0300

    refactor the code to have perimeters together and the areas together

--- learn_with_test/structs_methods_interfaces/structs_test.go
+++ learn_with_test/structs_methods_interfaces/structs_test.go
@@ -3,9 +3,18 @@ import ("testing"
 
 )
 func TestPerimeter(t *testing.T) {
-	got:=Perimeter(Rectangle{12.0,8.0})
+	t.Run("Rectangle perimeter",func(t *testing.T){
+		got:=Perimeter(Rectangle{12.0,8.0})
 	want:=40.0
 	assertMessage(t,got,want)
+	})
+
+
+		t.Run("Circle perimeter",func(t *testing.T){
+		got:=CirlePeri(Circle{3.5})
+	want:=21.994000
+	assertMessage(t,got,want)
+	})
 }
 
 func assertMessage(t testing.TB,want float64,got float64){
@@ -17,23 +26,17 @@ func assertMessage(t testing.TB,want float64,got float64){
 }
 
 func TestArea(t *testing.T){
-	got:=Area(Rectangle{5.0,6.0})
+	t.Run("Rectangle area",func(t *testing.T){
+got:=Area(Rectangle{5.0,6.0})
 	want:=30.0
 	assertMessage(t,got,want)
-}
-
-// Circle perimeter
-
-func TestCirlePeri(t *testing.T){
-	got:=CirlePeri(Circle{3.5})
-	want:=21.994000
-
-	assertMessage(t,got,want)
-}
-
-func TestCircleArea(t *testing.T){
-	got:=CirleArea(Circle{3.5})
+	})
+	
+t.Run("circle area",func(t *testing.T){
+got:=CirleArea(Circle{3.5})
 	want:=76.979000
 
 	assertMessage(t,got,want)
+	})
+	
 }
