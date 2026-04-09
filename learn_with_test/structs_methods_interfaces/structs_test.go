6ed60173930ce7a9d2f4b96a231e1f29e4fb0f00
evajuma <evansjuma1e@gmail.com>
2026-04-09 12:24:42 +0300

    modify precision from %f to %g which display all the decimal points

--- learn_with_test/structs_methods_interfaces/structs_test.go
+++ learn_with_test/structs_methods_interfaces/structs_test.go
@@ -1,42 +1,43 @@
 package main
-import ("testing"
 
+import (
+	"testing"
 )
+
 func TestPerimeter(t *testing.T) {
-	t.Run("Rectangle perimeter",func(t *testing.T){
-		got:=Perimeter(Rectangle{12.0,8.0})
-	want:=40.0
-	assertMessage(t,got,want)
+	t.Run("Rectangle perimeter", func(t *testing.T) {
+		got := Perimeter(Rectangle{12.0, 8.0})
+		want := 40.0
+		assertMessage(t, got, want)
 	})
 
-
-		t.Run("Circle perimeter",func(t *testing.T){
-		got:=CirlePeri(Circle{3.5})
-	want:=21.994000
-	assertMessage(t,got,want)
+	t.Run("Circle perimeter", func(t *testing.T) {
+		got := CirlePeri(Circle{float64(3.5)})
+		want := 21.991148575128552
+		assertMessage(t, got, want)
 	})
 }
 
-func assertMessage(t testing.TB,want float64,got float64){
+func assertMessage(t testing.TB, got float64, want float64) {
 	t.Helper()
 	if want != got {
-		t.Errorf("got %f,want %f",got,want)
+		t.Errorf("got %g,want %g", got, want)
 	}
 
 }
 
-func TestArea(t *testing.T){
-	t.Run("Rectangle area",func(t *testing.T){
-got:=Area(Rectangle{5.0,6.0})
-	want:=30.0
-	assertMessage(t,got,want)
+func TestArea(t *testing.T) {
+	t.Run("Rectangle area", func(t *testing.T) {
+		got := Area(Rectangle{5.0, 6.0})
+		want := 30.0
+		assertMessage(t, got, want)
 	})
-	
-t.Run("circle area",func(t *testing.T){
-got:=CirleArea(Circle{3.5})
-	want:=76.979000
 
-	assertMessage(t,got,want)
+	t.Run("circle area", func(t *testing.T) {
+		got := CirleArea(Circle{3.5})
+		want := 38.48451000647496
+
+		assertMessage(t, got, want)
 	})
-	
+
 }
