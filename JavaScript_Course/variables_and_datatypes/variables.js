307350d79fa59049587f73745d1bbf128cfbc37f
evajuma <evansjuma1e@gmail.com>
2026-04-20 11:07:52 +0300

    learning variables in js

---
+++ JavaScript_Course/variables_and_datatypes/variables.js
@@ -0,0 +1,7 @@
+var names  = "Juma";
+var courseName = "JavaScript";
+var isLoggedIn = true;
+var loggedIn = 34;
+var paymentMode; 
+paymentMode="Credit Card"
+console.log(names,courseName,isLoggedIn,loggedIn,paymentMode);
