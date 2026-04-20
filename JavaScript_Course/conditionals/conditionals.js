77ab913a7385403ea8f97651e464d88d801912f2
evajuma <evansjuma1e@gmail.com>
2026-04-20 13:59:16 +0300

    ternary conditionals in js,this is a short way of writing if ... else statement for certain conditions

--- JavaScript_Course/conditionals/conditionals.js
+++ JavaScript_Course/conditionals/conditionals.js
@@ -8,3 +8,13 @@ if (temperature<20){
 }else{
     console.log("Normal")
 }
+
+// ternary operator
+
+
+var authenticated = true;
+var count=false;
+
+authenticated && count ? console.log("SignOut"):console.log("Login"); 
+// the first  part shows the true condition while the second part is the false condition
+// the second part is the basically the fallback in case anything fails
