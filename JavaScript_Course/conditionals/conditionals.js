7943f88654a905de8c788dae280a6eee559f0831
evajuma <evansjuma1e@gmail.com>
2026-04-20 13:31:00 +0300

    conditionals if in js

---
+++ JavaScript_Course/conditionals/conditionals.js
@@ -0,0 +1,10 @@
+var temperature;
+temperature = 210;
+
+if (temperature<20){
+    console.log("It is cold outside");
+}else if (temperature>20){
+    console.log("Hot temperature")
+}else{
+    console.log("Normal")
+}
