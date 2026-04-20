b3a97a438dd9be280a47ffbe11b24172ca345dfc
evajuma <evansjuma1e@gmail.com>
2026-04-20 12:10:37 +0300

    simple user sign up with already provided information using the back ticks

---
+++ JavaScript_Course/userSignUp/sign.js
@@ -0,0 +1,16 @@
+const uid = "abc123";
+var fullName = "Evans Juma";
+var email = "evansjuma1@gmail.com";
+var password = "123456";
+var confirmPassword="123456";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+console.log(`
+    Full Name: ${fullName}
+    email: ${email}
+    password: ${password}
+    conPassword: ${confirmPassword}
+    course count: ${courseCount}
+    Login status: ${isLoggedInFromGoogle}
+    `);
+
