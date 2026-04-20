8d804bc4f64c4260ed54752064f3306d77144f85
evajuma <evansjuma1e@gmail.com>
2026-04-20 15:04:33 +0300

    adding a set of array to an existing array

--- JavaScript_Course/variables_and_datatypes/variables.js
+++ JavaScript_Course/variables_and_datatypes/variables.js
@@ -5,3 +5,22 @@ var loggedIn = 34;
 var paymentMode; 
 paymentMode="Credit Card"
 console.log(names,courseName,isLoggedIn,loggedIn,paymentMode);
+
+
+
+// various ways to add a set of aray to another array
+
+var users =['john','james']
+var newUser ='felix'
+
+//spread operator
+var allUsers=[...users,...newUser]
+console.log(allUsers)
+// push
+var allUsers=users.push(newUser)
+console.log(users)
+
+
+//pop removes an item from an array
+var popped = users.pop()
+console.log(popped)
