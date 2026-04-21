ed80ce94d45e5e3ee700ea08b5a8546e38048868
evajuma <evansjuma1e@gmail.com>
2026-04-21 10:28:07 +0300

    functions in js

---
+++ JavaScript_Course/functions/functions.js
@@ -0,0 +1,13 @@
+function sayHello(name){
+
+    console.log(`Hello there,${name}`);
+}
+
+sayHello("Juma");
+greetings=helloInSwahili();
+console.log(greetings);
+
+function helloInSwahili(){
+    return "Habari gani?"
+}
+
