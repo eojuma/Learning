d10e90129ec2edcaccc2f1b40434c25a6aaf9a0f
evajuma <evansjuma1e@gmail.com>
2026-04-22 18:32:10 +0100

    objects in js

---
+++ JavaScript_Course/objects/objects.js
@@ -0,0 +1,14 @@
+
+//object declaration and assignment
+var user = {
+  firstName: "Evans",
+  lastName: "Juma",
+  role: "Admin",
+  loginCount: 5,
+  facebookLogin: true,
+};
+
+//accessing values from an object
+console.log(user.firstName);
+console.log(user["lastName"]);
+
