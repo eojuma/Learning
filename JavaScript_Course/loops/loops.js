6b0bbd57a3e4d50966c73ec85cf6b1ef51068649
evajuma <evansjuma1e@gmail.com>
2026-04-27 21:51:48 +0100

    adde the while and do while loops

--- JavaScript_Course/loops/loops.js
+++ JavaScript_Course/loops/loops.js
@@ -1,7 +1,22 @@
 
 const myNumbers= ["one","Two","Three","Four","Five"]
-
+// for loop
 for (let index = 0; index<myNumbers.length; index++) {
     console.log(myNumbers[index]);
     
 }
+console.log()
+
+//  while loop
+let i=0;
+while(i<myNumbers.length){
+    console.log(myNumbers[i]);
+    i++;
+}
+console.log()
+//do while loop
+let j=0;
+do{
+    console.log(myNumbers[j]);
+    j++;
+}while(j<myNumbers.length)
