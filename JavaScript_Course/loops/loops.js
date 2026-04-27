63034d34f4c96fe97851aaa3ab967b21549b3587
evajuma <evansjuma1e@gmail.com>
2026-04-27 22:34:39 +0100

    added for each,for of and for in

--- JavaScript_Course/loops/loops.js
+++ JavaScript_Course/loops/loops.js
@@ -1,22 +1,47 @@
 
 const myNumbers= ["one","Two","Three","Four","Five"]
-// for loop
+// 1. for loop
 for (let index = 0; index<myNumbers.length; index++) {
     console.log(myNumbers[index]);
     
 }
 console.log()
 
-//  while loop
+//  2.while loop
 let i=0;
 while(i<myNumbers.length){
     console.log(myNumbers[i]);
     i++;
 }
-console.log()
-//do while loop
+console.log();
+// 3.do while loop
 let j=0;
 do{
     console.log(myNumbers[j]);
     j++;
-}while(j<myNumbers.length)
+}while(j<myNumbers.length);
+
+
+console.log();
+// 4.for each
+myNumbers.forEach((s)=>(console.log(s)));
+
+
+console.log()
+
+// 5.for of (arrats)
+for(const n of myNumbers){
+console.log(n);
+}
+const names={
+    it:"information technology",
+    yt:"youtube",
+    JSON:"JavaCript Object Notation"
+}
+console.log()
+// 6.for in  (objects)
+for(const a in names){
+console.log(a); // prints out the keys for the objects in js
+console.log(names[a]); // prints the values
+
+}
