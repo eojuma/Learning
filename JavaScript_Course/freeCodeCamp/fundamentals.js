29306be1286972b335165e7c831fa46243c16e8a
evajuma <evansjuma1e@gmail.com>
2026-06-17 17:52:57 +0300

    chore:  uding concat method to combine two or more strings

--- JavaScript_Course/freeCodeCamp/fundamentals.js
+++ JavaScript_Course/freeCodeCamp/fundamentals.js
@@ -8,3 +8,11 @@ var num = 1234567890123456789012345678901234567890n;
 console.log(num);
 let age;
 console.log(age);
+
+
+// using concat() method in js
+let fName = "Evans"
+let lName = "Juma"
+
+let fullName=fName.concat(' ',lName)
+console.log(fullName)
